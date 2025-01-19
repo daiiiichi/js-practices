810d3e396e87460d3997a418fc5f46167ef30663
daichitoyohara <daiiiichit@gmail.com>
2025-01-20 08:32:37 +0900

    2の課題で作成したfunction.jsを3の課題でも使用するために、自然なフォルダ構成、関数名に変更

--- 03.asynchronous/promise_async/function.js
+++ 03.asynchronous/promise_async/function.js
@@ -1,4 +1,4 @@
-export function runPromise(db, sql) {
+export function runAsync(db, sql) {
   return new Promise((resolve, reject) => {
     db.run(sql, function (err) {
       if (err) return reject(err);
@@ -7,7 +7,7 @@ export function runPromise(db, sql) {
   });
 }
 
-export function getPromise(db, sql) {
+export function getAsync(db, sql) {
   return new Promise((resolve, reject) => {
     db.get(sql, (err, row) => {
       if (err) return reject(err);
