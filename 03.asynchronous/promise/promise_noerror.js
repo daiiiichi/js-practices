7762fc8564b9f38587e7663b309b52c1d467f74b
daichitoyohara <daiiiichit@gmail.com>
2025-01-20 08:17:16 +0900

    1.callbackの修正に伴い、2.Promiseの課題を修正

--- 03.asynchronous/promise/promise_noerror.js
+++ 03.asynchronous/promise/promise_noerror.js
@@ -1,12 +1,27 @@
 import sqlite3 from "sqlite3";
-import { createTable, addRecord, getRecord, deleteTable } from "./function.js";
+import { runPromise, getPromise } from "./function.js";
 
 const db = new sqlite3.Database(":memory:");
 
-createTable(
+runPromise(
   db,
   "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
 )
-  .then(() => addRecord(db, "INSERT INTO books (title) VALUES ('Fight!')"))
-  .then(() => getRecord(db, "SELECT * FROM books"))
-  .then(() => deleteTable(db, "DELETE FROM books"));
+  .then(() => {
+    console.log("booksテーブルの作成に成功しました。");
+    return getPromise(db, "INSERT INTO books (title) VALUES ('Fight!')");
+  })
+  .then(() => {
+    console.log("レコードの追加に成功しました。");
+    return runPromise(db, "SELECT * FROM books");
+  })
+  .then(() => {
+    console.log("レコードの取得に成功しました。");
+    return runPromise(db, "DELETE FROM books");
+  })
+  .then(() => {
+    console.log("テーブルの削除に成功しました。");
+  })
+  .catch((err) => {
+    console.error("エラーが発生しました。", err);
+  });
