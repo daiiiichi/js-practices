fdcd9c145731196bd11c8dc9d5a030c3d37e673f
daichitoyohara <daiiiichit@gmail.com>
2025-01-20 20:25:49 +0900

    function.jsの関数名の変更に伴い、Promiseの関数の修正

--- 03.asynchronous/promise_async/promise_noerror.js
+++ 03.asynchronous/promise_async/promise_noerror.js
@@ -1,23 +1,23 @@
 import sqlite3 from "sqlite3";
-import { runPromise, getPromise } from "./function.js";
+import { runAsync, getAsync } from "./function.js";
 
 const db = new sqlite3.Database(":memory:");
 
-runPromise(
+runAsync(
   db,
   "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
 )
   .then(() => {
     console.log("booksテーブルの作成に成功しました。");
-    return getPromise(db, "INSERT INTO books (title) VALUES ('Fight!')");
+    return getAsync(db, "INSERT INTO books (title) VALUES ('Fight!')");
   })
   .then(() => {
     console.log("レコードの追加に成功しました。");
-    return runPromise(db, "SELECT * FROM books");
+    return runAsync(db, "SELECT * FROM books");
   })
   .then(() => {
     console.log("レコードの取得に成功しました。");
-    return runPromise(db, "DELETE FROM books");
+    return runAsync(db, "DELETE FROM books");
   })
   .then(() => {
     console.log("テーブルの削除に成功しました。");
