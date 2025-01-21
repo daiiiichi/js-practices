6d8c42a709967d6822d9bb0d8260ca372538dcdb
daichitoyohara <daiiiichit@gmail.com>
2025-01-21 22:43:07 +0900

    Dateオブジェクトをコンストラクタに移動

--- 04.class/memo_manager/filename.js
+++ 04.class/memo_manager/filename.js
@@ -1,13 +1,16 @@
 export class FileName {
+  constructor(date = new Date()) {
+    this.date = date;
+  }
+
   create() {
-    const now = new Date();
     const dateTimeComponents = [
-      now.getFullYear(),
-      now.getMonth() + 1,
-      now.getDate(),
-      now.getHours(),
-      now.getMinutes(),
-      now.getSeconds(),
+      this.date.getFullYear(),
+      this.date.getMonth() + 1,
+      this.date.getDate(),
+      this.date.getHours(),
+      this.date.getMinutes(),
+      this.date.getSeconds(),
     ];
 
     return dateTimeComponents
