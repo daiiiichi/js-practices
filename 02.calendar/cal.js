6fa9c9ec98224d4434a53ff20123fcb7bd620f55
daichitoyohara <daiiiichit@gmail.com>
2025-02-04 21:49:43 +0900

    条件演算子(`?`)をnull合体演算子(`??`)に修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -5,8 +5,8 @@ import minimist from "minimist";
 
 const input = minimist(process.argv.slice(2));
 const timeStamp = new Date();
-const year = input.y ? input.y : timeStamp.getFullYear();
-const month = input.m ? input.m : timeStamp.getMonth() + 1;
+const year = input.y ?? timeStamp.getFullYear();
+const month = input.m ?? timeStamp.getMonth() + 1;
 const calendar = calendarize(new Date(year, month - 1));
 
 const calendarName = `      ${month}月 ${year}`;
