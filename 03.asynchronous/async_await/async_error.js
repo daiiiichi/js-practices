b2daeaf06bf01625d57730751e6013345ea35542
daichitoyohara <daiiiichit@gmail.com>
2025-02-11 15:52:13 +0900

    ディレクトリ構成、フォルダ名の変更

--- 03.asynchronous/async_await/async_error.js
+++ 03.asynchronous/async_await/async_error.js
@@ -1,5 +1,5 @@
 import sqlite3 from "sqlite3";
-import { runAsync, getAsync } from "./function.js";
+import { runAsync, getAsync } from "../utils/async_util.js";
 
 const db = new sqlite3.Database(":memory:");
 
