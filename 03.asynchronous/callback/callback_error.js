d29c7c98025309a843e99df911ab7fb13956cf83
daichitoyohara <daiiiichit@gmail.com>
2025-02-11 13:17:29 +0900

    エラー想定以外の部分のエラーハンドリングを削除

--- 03.asynchronous/callback/callback_error.js
+++ 03.asynchronous/callback/callback_error.js
@@ -4,21 +4,17 @@ const db = new sqlite3.Database(":memory:");
 
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  (err) => {
-    if (err) return console.error("booksテーブルの作成に失敗しました。");
-    console.log("booksテーブルの作成に成功しました。");
-
+  () => {
     // Error: title(文字列型)に数値型を追加
     db.run("INSERT INTO books (title) VALUES 1", (err) => {
       if (err) return console.error("レコードの追加に失敗しました。");
       console.log("レコードの追加に成功しました。");
 
       db.get("SELECT * FROM books", (err, row) => {
-        if (err) return console.error("レコードの追加に失敗しました。");
+        if (err) return console.error("レコードの取得に失敗しました。");
         console.log("レコードの取得に成功しました。", row);
 
-        db.run("DELETE FROM books", (err) => {
-          if (err) return console.error("テーブルの削除に失敗しました。");
+        db.run("DELETE FROM books", () => {
           console.log("テーブルの削除に成功しました。");
         });
       });
--- 03.asynchronous/callback/callback_noError.js
+++ 03.asynchronous/callback/callback_noError.js
@@ -4,20 +4,16 @@ const db = new sqlite3.Database(":memory:");
 
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  (err) => {
-    if (err) return console.error("booksテーブルの作成に失敗しました。");
-    console.log("booksテーブルの作成に成功しました。");
-
+  () => {
     db.run("INSERT INTO books (title) VALUES ('Fight!')", (err) => {
       if (err) return console.error("レコードの追加に失敗しました。");
       console.log("レコードの追加に成功しました。");
 
       db.get("SELECT * FROM books", (err, row) => {
-        if (err) return console.error("レコードの追加に失敗しました。");
+        if (err) return console.error("レコードの取得に失敗しました。");
         console.log("レコードの取得に成功しました。", row);
 
-        db.run("DELETE FROM books", (err) => {
-          if (err) return console.error("テーブルの削除に失敗しました。");
+        db.run("DELETE FROM books", () => {
           console.log("テーブルの削除に成功しました。");
         });
       });
