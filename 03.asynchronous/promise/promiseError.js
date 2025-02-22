747eb618eac20bed256d4b1850e282afee651ca8
daichitoyohara <daiiiichit@gmail.com>
2025-02-22 11:00:43 +0900

    camel caseへの統一

--- 03.asynchronous/promise/promiseError.js
+++ 03.asynchronous/promise/promiseError.js
@@ -1,5 +1,5 @@
 import sqlite3 from "sqlite3";
-import { runAsync, getAsync } from "../utils/async_util.js";
+import { runAsync, getAsync } from "../utils/asyncUtil.js";
 
 const db = new sqlite3.Database(":memory:");
 
