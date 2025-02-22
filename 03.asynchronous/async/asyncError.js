f813e5c436dfb687909a4ba6e1af656ff072c6ef
daichitoyohara <daiiiichit@gmail.com>
2025-02-22 11:10:07 +0900

    適切なファイル名へ変更　asyncUtil.js -> sqlFunctions.js

--- 03.asynchronous/async/asyncError.js
+++ 03.asynchronous/async/asyncError.js
@@ -1,5 +1,5 @@
 import sqlite3 from "sqlite3";
-import { runAsync, getAsync } from "../utils/asyncUtil.js";
+import { runAsync, getAsync } from "../utils/sqlFunctions.js";
 
 const db = new sqlite3.Database(":memory:");
 
