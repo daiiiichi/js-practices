93ae06fcf43ebcb7d829eea057a5a0701b4566be
daichitoyohara <daiiiichit@gmail.com>
2025-02-22 12:08:49 +0900

    async関数の実装から、Top-level awaitへの実装に変更

--- 03.asynchronous/async/asyncError.js
+++ 03.asynchronous/async/asyncError.js
@@ -3,31 +3,24 @@ import { runAsync, getAsync } from "../utils/sqlFunctions.js";
 
 const db = new sqlite3.Database(":memory:");
 
-async function main() {
-  await runAsync(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
+await runAsync(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 
-  try {
-    const result = await runAsync(
-      db,
-      "INSERT INTO books (title) VALUES (NULL)",
-    ); // Error: title(文字列型)にNULLを追加
-    console.log("レコードの追加に成功しました。id:", result.lastID);
-  } catch (err) {
-    console.error("レコードの追加に失敗しました。ERROR MESSAGE:", err.message);
-  }
-
-  try {
-    const row = await getAsync(db, "SELECT * FROM refs"); // Error: 存在しないテーブルの参照
-    console.log("レコードの取得に成功しました。", row);
-  } catch (err) {
-    console.error("レコードの取得に失敗しました。ERROR MESSAGE:", err.message);
-  }
-
-  await runAsync(db, "DROP TABLE books");
-  console.log("テーブルの削除に成功しました。");
+try {
+  const result = await runAsync(db, "INSERT INTO books (title) VALUES (NULL)"); // Error: title(文字列型)にNULLを追加
+  console.log("レコードの追加に成功しました。id:", result.lastID);
+} catch (err) {
+  console.error("レコードの追加に失敗しました。ERROR MESSAGE:", err.message);
 }
 
-main();
+try {
+  const row = await getAsync(db, "SELECT * FROM refs"); // Error: 存在しないテーブルの参照
+  console.log("レコードの取得に成功しました。", row);
+} catch (err) {
+  console.error("レコードの取得に失敗しました。ERROR MESSAGE:", err.message);
+}
+
+await runAsync(db, "DROP TABLE books");
+console.log("テーブルの削除に成功しました。");
--- 03.asynchronous/async/asyncNoError.js
+++ 03.asynchronous/async/asyncNoError.js
@@ -3,23 +3,19 @@ import { runAsync, getAsync } from "../utils/sqlFunctions.js";
 
 const db = new sqlite3.Database(":memory:");
 
-async function main() {
-  await runAsync(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
+await runAsync(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 
-  const result = await runAsync(
-    db,
-    "INSERT INTO books (title) VALUES ('Fight!')",
-  );
-  console.log("レコードの追加に成功しました。id:", result.lastID);
+const result = await runAsync(
+  db,
+  "INSERT INTO books (title) VALUES ('Fight!')",
+);
+console.log("レコードの追加に成功しました。id:", result.lastID);
 
-  const row = await getAsync(db, "SELECT * FROM books");
-  console.log("レコードの取得に成功しました。", row);
+const row = await getAsync(db, "SELECT * FROM books");
+console.log("レコードの取得に成功しました。", row);
 
-  await runAsync(db, "DROP TABLE books");
-  console.log("テーブルの削除に成功しました。");
-}
-
-main();
+await runAsync(db, "DROP TABLE books");
+console.log("テーブルの削除に成功しました。");
