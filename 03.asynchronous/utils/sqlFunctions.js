f140e3063b98582d88f5f724be20794af5b89662
daichitoyohara <daiiiichit@gmail.com>
2025-02-22 11:41:34 +0900

    if文のブロックを正しく記入

--- 03.asynchronous/utils/sqlFunctions.js
+++ 03.asynchronous/utils/sqlFunctions.js
@@ -1,8 +1,11 @@
 export function runAsync(db, sql) {
   return new Promise((resolve, reject) => {
     db.run(sql, function (err) {
-      if (err) return reject(err);
-      resolve(this);
+      if (err) {
+        return reject(err);
+      } else {
+        resolve(this);
+      }
     });
   });
 }
@@ -10,8 +13,11 @@ export function runAsync(db, sql) {
 export function getAsync(db, sql) {
   return new Promise((resolve, reject) => {
     db.get(sql, (err, row) => {
-      if (err) return reject(err);
-      resolve(row);
+      if (err) {
+        return reject(err);
+      } else {
+        resolve(row);
+      }
     });
   });
 }
