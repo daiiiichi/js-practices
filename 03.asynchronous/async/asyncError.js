93786649340be56aff16cdac53cf17d15e9306b5
daichitoyohara <daiiiichit@gmail.com>
2025-02-24 22:06:54 +0900

    捕捉したい例外のみを捕捉し、それ以外の例外は上位に伝播するように修正

--- 03.asynchronous/async/asyncError.js
+++ 03.asynchronous/async/asyncError.js
@@ -3,24 +3,39 @@ import { runAsync, getAsync } from "../utils/sqlFunctions.js";
 
 const db = new sqlite3.Database(":memory:");
 
-await runAsync(
-  db,
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-);
-
 try {
-  const result = await runAsync(db, "INSERT INTO books (title) VALUES (NULL)"); // Error: title(文字列型)にNULLを追加
-  console.log("レコードの追加に成功しました。id:", result.lastID);
+  await runAsync(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+
+  try {
+    const result = await runAsync(
+      db,
+      "INSERT INTO books (title) VALUES (NULL)",
+    ); // Error: title(文字列型)にNULLを追加
+    console.log("レコードの追加に成功しました。id:", result.lastID);
+  } catch (err) {
+    if (err.message.startsWith("SQLITE_")) {
+      console.error(err.message);
+    } else {
+      throw err;
+    }
+  }
+
+  try {
+    const row = await getAsync(db, "SELECT * FROM refs"); // Error: 存在しないテーブルの参照
+    console.log("レコードの取得に成功しました。", row);
+  } catch (err) {
+    if (err.message.startsWith("SQLITE_")) {
+      console.error(err.message);
+    } else {
+      throw err;
+    }
+  }
+
+  await runAsync(db, "DROP TABLE books");
+  console.log("テーブルの削除に成功しました。");
 } catch (err) {
   console.error(err.message);
 }
-
-try {
-  const row = await getAsync(db, "SELECT * FROM refs"); // Error: 存在しないテーブルの参照
-  console.log("レコードの取得に成功しました。", row);
-} catch (err) {
-  console.error(err.message);
-}
-
-await runAsync(db, "DROP TABLE books");
-console.log("テーブルの削除に成功しました。");
