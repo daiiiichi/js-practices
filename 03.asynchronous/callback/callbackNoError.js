76faa3efaa42791ce607e34feee6467fd9f3ec92
daichitoyohara <daiiiichit@gmail.com>
2025-03-02 16:15:20 +0900

    callbackNoError.jsのエラー修正

--- 03.asynchronous/callback/callbackNoError.js
+++ 03.asynchronous/callback/callbackNoError.js
@@ -5,10 +5,10 @@ const db = new sqlite3.Database(":memory:");
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   () => {
-    db.run("INSERT INTO books (title) VALUES ('Fight!')", () => {
+    db.run("INSERT INTO books (title) VALUES ('Fight!')", function () {
       console.log("レコードの追加に成功しました。id:", this.lastID);
 
-      db.get("SELECT * FROM books", (row) => {
+      db.get("SELECT * FROM books", (err, row) => {
         console.log("レコードの取得に成功しました。", row);
 
         db.run("DROP TABLE books", () => {
