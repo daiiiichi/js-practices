ee2704bd0781a71f53ce4af37d256e99587ac7eb
daichitoyohara <daiiiichit@gmail.com>
2025-03-15 14:44:54 +0900

    asyncエラー有のtry...catch文の修正

--- 03.asynchronous/async/asyncError.js
+++ 03.asynchronous/async/asyncError.js
@@ -3,31 +3,23 @@ import { runAsync, getAsync } from "../utils/sqlFunctions.js";
 
 const db = new sqlite3.Database(":memory:");
 
-try {
-  await runAsync(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
+await runAsync(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 
+try {
   const result = await runAsync(db, "INSERT INTO books (title) VALUES (NULL)"); // Error: title(文字列型)にNULLを追加
   console.log("レコードの追加に成功しました。id:", result.lastID);
 } catch (err) {
-  if (err.message.startsWith("SQLITE_")) {
-    console.error(err.message);
-  } else {
-    throw err;
-  }
+  console.error(err.message);
 }
 
 try {
   const row = await getAsync(db, "SELECT * FROM refs"); // Error: 存在しないテーブルの参照
   console.log("レコードの取得に成功しました。", row);
 } catch (err) {
-  if (err.message.startsWith("SQLITE_")) {
-    console.error(err.message);
-  } else {
-    throw err;
-  }
+  console.error(err.message);
 }
 
 await runAsync(db, "DROP TABLE books");
