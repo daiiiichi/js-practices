14fffefc8db6ff74f15b7d4a0b50663dad9bee8a
daichitoyohara <daiiiichit@gmail.com>
2025-03-27 23:00:26 +0900

    エラーハンドリングの条件を特定のエラーコードにのみ捕捉するように修正

--- 03.asynchronous/async/asyncError.js
+++ 03.asynchronous/async/asyncError.js
@@ -12,7 +12,11 @@ try {
   const result = await runAsync(db, "INSERT INTO books (title) VALUES (NULL)"); // Error: title(文字列型)にNULLを追加
   console.log("レコードの追加に成功しました。id:", result.lastID);
 } catch (err) {
-  if (err && typeof err === "object" && "code" in err) {
+  if (
+    typeof err === "object" &&
+    err.code === "SQLITE_CONSTRAINT" &&
+    err.message.includes("NOT NULL constraint failed")
+  ) {
     console.error(err.message);
   } else {
     throw err;
@@ -23,7 +27,11 @@ try {
   const row = await getAsync(db, "SELECT * FROM refs"); // Error: 存在しないテーブルの参照
   console.log("レコードの取得に成功しました。", row);
 } catch (err) {
-  if (err && typeof err === "object" && "code" in err) {
+  if (
+    typeof err === "object" &&
+    err.code === "SQLITE_ERROR" &&
+    err.message.includes("no such table")
+  ) {
     console.error(err.message);
   } else {
     throw err;
